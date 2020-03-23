1a29478b47a085c629102bc13994f01403b30bdb
Akinrolabu Lolade <busayoololade@yahoo.com>
2020-03-23 23:21:20 +0100

    Added new file@

---
+++ contact/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> Contact Form</title>
+</head>
+<body>
+<header>
+  <h1> Contact Us </h1>
+</header>
+<p> </p>
+<form action="processform.php" method="POST">
+<p><label for="firstname"> First Name<span> * </span></label>
+<input type="text" name="firstname" id="firstname" >
+</p>
+<p>
+<label for="lastname"> Last Name</label>
+<input type="text" name="lastname" id="lastname" >
+</p>
+<p>
+<label for="surname"> Surname <span> * </span></label>
+<input type="text" name="surname" id="surname" >
+</p>
+<p>
+<label for="gender"> Gender <span> * </span></label>
+<input type="radio" name="gender" value="male" > Male
+<input type="radio" name="gender" value="female" > Female
+</p>
+<p>
+  <label> Select your reason</label>
+  <select name="reason">
+    <option> Select your reason</option>
+    <option value="job"> For a Job</option>
+    <option value="Personal">  Personal </option>
+    <option value="mentorship"> Mentorship </option>
+  </select>
+</p>
+<p>
+<label for="comments"> Comments<span> * </span> </label>
+<br/>
+<textarea name="comments">
+</textarea>
+</p>
+<p>
+  <input type="checkbox" name="agree" >By checking this box you agree to our terms and condition
+</p>
+<button type="submit" > Submit</button>
+</form>
+
+
+
+
+</body>
+</html>
---
+++ contact/processform.php
@@ -0,0 +1,36 @@
+<?php
+
+$firstname=$_POST["firstname"];
+$lastname=$_POST["lastname"];
+$surname=$_POST["surname"];
+$gender=$_POST["gender"];
+$reason=$_POST["reason"];
+$comments=$_POST["comments"];
+$agree=$_POST["agree"];
+
+
+$filepath ="C:\wamp64\www\contact\\$firstname.txt";
+$file = fopen( $filepath, "w" );
+
+if($file == false){
+  print_r("Am not here oooo");
+  echo ("Error in opening new file");
+  exit();
+}else{
+  
+  print_r("Here oooooo");
+  $msg=" First Name: $firstname\n 
+  Surname :$surname\n
+  Last Name:$lastname\n
+  Gender :$gender\n
+  Reason:$reason\n
+  Comments:$comments";
+
+  fwrite($file,$msg);
+  fclose($file);
+
+  header("Location:index.php");
+  
+}
+
+?>
